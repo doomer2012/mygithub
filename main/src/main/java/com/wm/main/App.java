4a5d24579e6f1e066a86de7ca8b7da1117b811fc
doomer2012 <767182049@qq.com>
2016-09-12 17:07:22 +0800

    Add files via upload

---
+++ main/src/main/java/com/wm/main/App.java
@@ -0,0 +1,19 @@
+package com.wm.main;
+
+/**
+ * 
+ * @description
+ * 
+ * @author weiming
+ * 
+ * @data 2016年9月12日
+ */
+public class App {
+
+    public App() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
